707e9178c9875cc5c226bff00958ad97888985d2
Bohdan Sukhomlinov <bsukho@hungary.softserveinc.com>
2019-09-27 10:48:00 +0300

    hello kb61m

---
+++ README.md
@@ -0,0 +1,13 @@
+# SIMPLE pyTASKER SAMPLE
+## What
+* create/modify/delete task for day
+* create/modify/delete task for lesson (1st, 2nd, etc)
+* show task for today (agenda)
+* show task for day
+## Why
+To be a sample of easy python
+## How
+check inline docs:
+python tasker.py --help
+## Who
+shellshock.dnull[at]gmail.com
---
+++ tasker.py
@@ -0,0 +1,87 @@
+import argparse
+import json  # We use JSON to store date to file
+import datetime
+
+
+def get_current_date(format):
+    # Python is well know
+    # for hard work with time
+    # https://www.programiz.com/python-programming/datetime/current-datetime
+    now = datetime.datetime.now()
+    return now.strftime(format)
+
+def get_args():
+    parser = argparse.ArgumentParser(description='pyTasker - easy tasker sample')
+    parser.add_argument(
+        '--create',
+        help="Interactive task creation"
+    )
+    parser.add_argument(
+        '--modify',
+        help="Interactive task modification"
+    )
+    parser.add_argument(
+        '--delete',
+        help="Interactive task deletion"
+    )
+    parser.add_argument(
+        '--show',
+        help="Show current agenda"
+    )
+    parser.add_argument(
+        '--show-date',
+        help="Show agenda for some date"
+    )
+    args = parser.parse_args()
+    return args
+
+def get_db():
+    try:
+        # If data available and we can read
+        # we convert is to JSON format
+        with open(DB_FILE, 'r') as db_read_file:
+            db = json.load(db_read_file)
+        return db
+    except ValueError:
+        # But!
+        # If it is empty, will get Error: Cant load empty data!
+        # If so, we will catch those error, and Handle It by using {}
+        # empty dict. Empty dict is not empty data, and json.loads
+        # knows how to read it!
+        print "[!!] No data in DB."
+        return {}
+
+def create():
+    # Input: https://www.geeksforgeeks.org/taking-input-in-python/
+    name = str(raw_input("Name of task:"))
+    date = str(raw_input("Date of task:"))
+    db[date] = name
+
+
+def main():
+    args = get_args()
+
+    db = get_db()
+    db["today"] = TODAY
+
+    if args.create:
+        create(db)
+
+
+    print db
+    with open(DB_FILE, 'w') as db_write_file:
+        json.dump(db, db_write_file, sort_keys=True, indent=4)
+    #print(dump_db)
+
+    # f.write(dump_db)
+    # f.close()
+
+
+# Config
+DB_FILE='tasker.db'
+FORMAT="%Y/%m/%d"# %H:%M:%S"
+TODAY=get_current_date(FORMAT)
+
+if __name__ == '__main__':
+    print("pyTasker")
+    main()
